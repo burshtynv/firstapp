18c2d0d94917eb2c48d020faeb0f10718ecb03f4
burshtynv <vanyaburshtyn@gmail.com>
2021-07-27 10:10:47 +0300

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {}
+
+class MyFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  }
+}
