8e5e8c4bd16d855fc3a374707d159a8732e6f07e
burshtynv <vanyaburshtyn@gmail.com>
2021-07-28 16:43:33 +0300

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,22 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyFirstApp());
-
-class MyFirstApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main() {
+  runApp(MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                LinearProgressIndicator(value: 23),
-                Text(
-                  '23%',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-                Text(
-                  'Press button to download',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-              ],
-            ),
-          ),
-        ),
-        backgroundColor: Colors.indigo,
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.cloud_download),
-        ),
+    appBar: AppBar(
+      title: Text('MyFirstApp'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text(
+        'wefekgjnreglmgk',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            fontWeight: FontWeight.bold, color: Colors.red, fontSize: 40),
       ),
-    );
-  }
+    ),
+  )));
 }
